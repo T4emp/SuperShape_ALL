3b8990f06f0128e475cdc5aadeb144ec0dd86769
T4emp <sergey63777@mail.ru>
2022-03-11 22:30:20 +0300

    Initial commit

---
+++ src/com/xwx/Square.java
@@ -0,0 +1,43 @@
+package com.xwx;
+
+public class Square extends Rectangle{
+    private double side;
+
+    public Square(){
+        this.side = side;
+    }
+
+    public Square(double side){
+        this.side = side;
+    }
+
+    public Square(String color, boolean filled, double width, double length, double side) {
+        super(color, filled, width, length);
+        this.side = side;
+    }
+
+    public double getSide() {
+        return side;
+    }
+
+    public void setSide(double side) {
+        this.side = side;
+    }
+
+    @Override
+    public void setWidth(double width) {
+        super.setWidth(width);
+    }
+
+    @Override
+    public void setLength(double length) {
+        super.setLength(length);
+    }
+
+    @Override
+    public String toString() {
+        return "Square{" +
+                "side=" + side +
+                '}';
+    }
+}
